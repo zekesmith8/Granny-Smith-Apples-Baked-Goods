421b25f3d5ffaec8ee917f29ed15f669840ea8df
Zeke <zekesmith8@gmail.com>
2023-05-09 13:11:02 -0700

    the base start of things

--- src/_codux/boards/app/market.board.tsx
+++ src/_codux/boards/app/market.board.tsx
@@ -1,6 +1,7 @@
 import { createBoard } from '@wixc3/react-board';
+import App from '../../../App';
 
 export default createBoard({
     name: 'Market',
-    Board: () => <div></div>
+    Board: () => <App />
 });
